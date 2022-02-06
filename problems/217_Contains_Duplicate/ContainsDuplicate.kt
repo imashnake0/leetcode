3619efe50f2d3e95b2fbea7f30ddb778949f3952
Kamalesh Reddy Paluru <kamaleshpaluru07@gmail.com>
2022-02-05 19:15:53 -0500

    Solved Contains Duplicate
    Solved the 1st problem in Leetcode Patterns.

---
+++ problems/217_Contains_Duplicate/ContainsDuplicate.kt
@@ -0,0 +1,45 @@
+// Attempt 1
+/*
+class ContainsDuplicate() {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        var sum = 0
+        for (i in 0 until nums.size) {
+            for (j in 0 until nums.size) {
+                if(nums[i] == nums[j]) {
+                    sum++
+                }
+            }
+        }
+        if (sum > nums.size) {
+            return true
+        }
+        return false
+    }
+}
+*/
+
+// Attempt 2
+/*
+class ContainsDuplicate {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val numsSorted = nums.sorted()
+        for (i in 0 until numsSorted.lastIndex) {
+            if (numsSorted[i] == numsSorted[i + 1]) {
+                return true
+            }
+        }
+        return false
+    }
+}
+*/
+
+// Attempt 3
+class ContainsDuplicate {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val numsSet = nums.toSet()
+        if(numsSet.size == nums.size) {
+            return false
+        }
+        return true
+    }
+}
